3b6dca6962072d4fddd6d12d19c6394eda52ef4e
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-03 12:35:41 +0300

    added cache-first strategy

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-self.addEventListener('activate', event => {
+const staticCacheName = 'js-pwa-app-v1'
+
+const assetUrls = [
+  'index.html',
+  '/js/app.js',
+  '/css/styles.css',
+]
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)
+})
+
+self.addEventListener('activate', async event => {
   console.log('activate')
 })
 
-self.addEventListener('install', event => {
-  console.log('install')
+self.addEventListener('fetch',  event => {
+  console.log('Fetch', event.request.url)
+  event.respondWith(cacheFirst(event.request))
 })
 
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
+}
