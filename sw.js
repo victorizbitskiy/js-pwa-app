fc930b8c64f378b443051232a3934810b373b3b5
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-05 20:06:18 +0300

    added network-first strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'js-pwa-app-v4'
+const staticCacheName = 's-js-pwa-app-v4'
+const dynamicCacheName = 'd-js-pwa-app-v4'
 
 const assetUrls = [
   'index.html',
@@ -15,17 +16,37 @@ self.addEventListener('activate', async event => {
   const cacheNames = await caches.keys()
   await Promise.all(
     cacheNames
-    .filter(name => name !== staticCacheName)
-    .map(name => caches.delete(name))
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+      .map(name => caches.delete(name))
   )
 })
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch', event.request.url)
-  event.respondWith(cacheFirst(event.request))
+  const { request } = event
+  const url = new URL(request.url)
+
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request))
+  } else {
+    event.respondWith(networkFirst(request))
+  }
+
 })
 
 async function cacheFirst(request) {
   const cached = await caches.match(request)
   return cached ?? await fetch(request)
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (e) {
+    const cached = await cache.match(request)
+    return cached ?? caches.match('/offline.html')
+  }
 }
